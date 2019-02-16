c184040ac681dee97f0d8ca2ff3006fd60a8a81c
wxie <wxie11@hawk.iit.edu>
2019-02-15 18:18:04 -0600

    write migration for the join table

--- db/migrate/20190216001509_create_difficulties_recipes_join_table.rb
+++ db/migrate/20190216001509_create_difficulties_recipes_join_table.rb
@@ -1,4 +1,8 @@
 class CreateDifficultiesRecipesJoinTable < ActiveRecord::Migration[5.2]
   def change
+    create_join_table :difficulties, :recipes do |t|
+      t.index :difficulty_id
+      t.index :recipe_id
+    end
   end
 end
