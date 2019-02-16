3dc9fa04bf4fb799d015c073c7d641a1a5974c27
wxie <wxie11@hawk.iit.edu>
2019-02-15 22:48:41 -0600

    Add seed data
      $ rake db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Recipe.destroy_all
+Difficulty.destroy_all
+
+@easy = Difficulty.create(
+  level: 'easy'
+)
+
+@moderate = Difficulty. create(
+  level: 'moderate'
+)
+
+@challenging = Difficulty.create(
+  level: 'challenging'
+)
+
+Recipe.create!(title: 'Shrimp Paste',
+  img_url: 'shrimp_pasta.jpg',
+  ingredient: 'Shrimp, Pasta, Butter, Olive Oil, Garlic',
+  direction:
+    %{<p>
+      First cook pasta in boil water for 6-8 minutes. Then
+      cook the shrimp with butter, garlic, and oil on a hot
+      pan for 3 minutes. Finally put the pasta into the pan,
+      cook for 5 mintues.
+      </p>},
+  difficulty_id: '3')
+
+Recipe.create!(title: 'Poke Bowl',
+  img_url: 'poke_bowl.jpg',
+  ingredient: 'Tuna, salmon, rice, vinger, soy sauce',
+  direction:
+    %{<p>
+      First mixes the rice with vinger in a bowl. Then place all
+      the fish on top of the rice. Finally, put some soy sauce.
+      </p>},
+  difficulty_id: '2')
+
+Recipe.create!(title: 'Instant Ramen',
+  img_url: 'ramen.jpg',
+  ingredient: 'Cup noodle, water',
+  direction:
+    %{<p>
+      First boil the water. Then put the boiled water into the cup
+      noodle. Wait for 3 minutes!
+      </p>},
+  difficulty_id: '1')
