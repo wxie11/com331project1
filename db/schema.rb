2672891e2e7644656d57ac2114859cd969aa34e1
wxie <wxie11@hawk.iit.edu>
2019-02-15 18:14:01 -0600

    run Difficulty migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_000844) do
+ActiveRecord::Schema.define(version: 2019_02_16_001227) do
+
+  create_table "difficulties", force: :cascade do |t|
+    t.string "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
