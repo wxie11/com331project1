8af936f57cf61741f0b8a3851166c60696bd661f
wxie <wxie11@hawk.iit.edu>
2019-02-15 19:25:25 -0600

    rollback migration

--- db/migrate/20190216001509_create_difficulties_recipes_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateDifficultiesRecipesJoinTable < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :difficulties, :recipes do |t|
-      t.index :difficulty_id
-      t.index :recipe_id
-    end
-  end
-end
